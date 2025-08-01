87352029b4ebe36380270bc18313449fe45b4f12
gothmiku <berkan10001@gmail.com>
2025-08-01 16:48:26 +0300

    Viewmodels W.I.P

--- app/src/main/java/com/example/weatherapp/presentation/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/presentation/viewmodel/WeatherViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp.presentation.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,16 +10,34 @@ import com.example.weatherapp.data.model.WeatherInfo
 import kotlinx.coroutines.flow.Flow
 import com.example.weatherapp.data.repo.WeatherRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
-class WeatherAppViewModel @Inject constructor (private val repo : WeatherRepo) : ViewModel() {
+class WeatherAppViewModel @Inject constructor(private val repo: WeatherRepo) : ViewModel() {
     val allWeatherInfos: Flow<List<WeatherInfo>> = repo.allWeatherInfos
 
     fun insertWeatherInfo(weatherInfo: WeatherInfo) {
-        viewModelScope.launch {
-            repo.insertWeatherInfo(weatherInfo)
+        viewModelScope.launch(Dispatchers.IO) {
+            if (repo.getWeatherInfoByDate(LocalDate.now().toString()) == weatherInfo) {
+                Log.d("ViewModel","It already exists")
+            }else{
+                repo.insertWeatherInfo(weatherInfo)
+            }
+        }
+    }
+
+
+    //TODO
+    fun checkForUpdate(date : String){
+        viewModelScope.launch(Dispatchers.IO) {
+            val oldestWeatherInfo = repo.getLatestWeatherInfo()
+            if(oldestWeatherInfo.date == date){
+
+            }
+
         }
     }
 
@@ -38,7 +57,7 @@ class WeatherAppViewModel @Inject constructor (private val repo : WeatherRepo) :
         return repo.getWeatherInfoCount()
     }
 
-    suspend fun update(weatherInfo: WeatherInfo){
+    suspend fun update(weatherInfo: WeatherInfo) {
         repo.update(weatherInfo)
     }
 
