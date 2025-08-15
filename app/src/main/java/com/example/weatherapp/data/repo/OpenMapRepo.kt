5f0015ad6fc7c486bf4cac08f69ad45c57ab565f
gothmiku <berkan10001@gmail.com>
2025-08-15 08:49:10 +0300

    Bug fixes

--- app/src/main/java/com/example/weatherapp/data/repo/OpenMapRepo.kt
+++ app/src/main/java/com/example/weatherapp/data/repo/OpenMapRepo.kt
@@ -1,19 +1,24 @@
 package com.example.weatherapp.data.repo
 
 import android.util.Log
+import com.example.weatherapp.data.model.Address
 import com.example.weatherapp.data.model.AddressResponse
 import com.example.weatherapp.data.remote.OpenMapAPI
 import retrofit2.Response
 import javax.inject.Inject
 
 class OpenMapRepo @Inject constructor(private val api: OpenMapAPI) {
+
+    val noResponseAddress = Address("API Error", "API Error", "Default")
+    val noResponse = AddressResponse(noResponseAddress)
+
     suspend fun getJSON(lat : Float, lon : Float) : AddressResponse{
         val response = api.getJSON(lat,lon)
         if (response.isSuccessful) {
             return response.body()!!
         }else{
             Log.e("WeatherRepo", "Error Code: ${response.code()}")
-            throw Exception("Error fetching today's weather")
+            return noResponse
         }
     }
 
@@ -23,7 +28,7 @@ class OpenMapRepo @Inject constructor(private val api: OpenMapAPI) {
             return response
         }else{
             Log.e("WeatherRepo", "Error Code: ${response.code()}")
-            throw Exception("Error fetching today's weather")
+            throw Exception("Error fetching location info")
         }
     }
 }
